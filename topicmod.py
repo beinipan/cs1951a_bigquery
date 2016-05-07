654d3b4bdb17e7f41d200702c5be1a05968b0b01
beinipan <beini_pan@brown.edu>
2016-05-07 10:02:09 -0400

    small topicmod example

---
+++ topicmod.py
@@ -0,0 +1,15 @@
+#example i found online, only problem is we have to make the vocab list? or something, or we can use a count vectorizer thing i guess? 
+
+import numpy as np
+import lda
+import lda.datasets
+X = lda.datasets.load_reuters()
+vocab = lda.datasets.load_reuters_vocab()
+
+model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
+model.fit(X)  # model.fit_transform(X) is also available
+topic_word = model.topic_word_  # model.components_ also works
+n_top_words = 8
+for i, topic_dist in enumerate(topic_word):
+	topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
+	print('Topic {}: {}'.format(i, ' '.join(topic_words)))
