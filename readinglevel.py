d017865d2f1febebcfb63f89666a62a33a9a7736
beinipan <beini_pan@brown.edu>
2016-05-07 09:45:01 -0400

    reading level

---
+++ readinglevel.py
@@ -0,0 +1,26 @@
+from textstat.textstat import textstat
+import re
+import time
+import csv
+
+start_time = time.time()
+
+def load_file(file_path):
+    comments = []
+    with open(file_path, 'r') as file_reader:
+        reader = csv.reader(file_reader, delimiter=',', quotechar='"')
+        reader.next()
+        for row in reader:
+            text = re.sub('([A-Za-z]+:\/\/[A-Za-z0-9]+\.[A-Za-z0-9]+[^\s-]*)|([A-Za-z]+\.[A-Za-z0-9]+\.[A-Za-z0-9]+[^\s-]*)', '', row[0]) #url get rid
+            text = re.sub('\s\s+', ' ', text)
+            comments.append(text)
+    return comments
+
+documents = load_file("worldnewsFilteredQuery.csv")
+
+levels = [textstat.flesch_reading_ease(comment) for comment in documents if textstat.sentence_count(comment) != 0]
+
+print sum(levels)/len(levels)
+
+elapsed_time = time.time() - start_time
+print "elapsed time in seconds: " + str(elapsed_time)
